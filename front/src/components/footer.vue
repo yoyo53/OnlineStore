a90292e8020c83894cccffd0ab51ed4c459b45be
Hugo-transverse <80055149+Hugo-transverse@users.noreply.github.com>
2023-06-25 14:36:22 +0200

    footer logo clicable to home page

--- front/src/components/footer.vue
+++ front/src/components/footer.vue
@@ -3,7 +3,10 @@
     <div class="mx-auto max-w-screen-xl px-4 py-2 sm:px-6 lg:px-0 ">
       <div class="sm:flex sm:items-center sm:justify-between">
         <div class="flex justify-center text-teal-600 sm:justify-start">
-          <img src="..\assets\logo-white.png">
+          <router-link to="/home" class="flex">
+            <span class="sr-only">Logo</span>
+            <img src="..\assets\logo-white.png" alt="logo" class="logo">
+          </router-link>
         </div>
 
         <p class="mt-4 text-center text-sm text-white lg:mt-0 lg:text-right">
--- front/src/views/SignIn.vue
+++ front/src/views/SignIn.vue
@@ -145,7 +145,22 @@
       </div>
   </section>
 
-  <Footer></Footer>
+  <footer class="fixed bottom-0 left-0 w-full bg-[#007EA7]">
+    <div class="mx-auto max-w-screen-xl px-4 py-2 sm:px-6 lg:px-0 ">
+      <div class="sm:flex sm:items-center sm:justify-between">
+        <div class="flex justify-center text-teal-600 sm:justify-start">
+          <router-link to="/" class="flex">
+            <span class="sr-only">Logo</span>
+            <img src="..\assets\logo-white.png" alt="logo" class="logo">
+          </router-link>
+        </div>
+
+        <p class="mt-4 text-center text-sm text-white lg:mt-0 lg:text-right">
+          Copyright &copy; 2023. All rights reserved.
+        </p>
+      </div>
+    </div>
+  </footer>
 
 </template>
 
