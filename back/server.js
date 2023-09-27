bedff13a57a8c285d072648d29b122a4b37cc5bd
Yohan Villiers <94387950+yoyo53@users.noreply.github.com>
2023-09-27 12:16:09 +0200

    Update server.js
    update cors policy

--- back/server.js
+++ back/server.js
@@ -8,7 +8,7 @@ const app = express()
 const port = process.env.PORT ?? 3000
 
 app.use(cors({
-  "origin": "https://cyb0nix.github.io",
+  "origin": "*",
   "methods": "GET,PATCH,POST,PUT,DELETE,OPTIONS",
   "allowedHeaders": "X-Requested-With,Content-Type,Authorization"
 }))
